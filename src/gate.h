b7e786a756277b567ac486d5ce6c0e5b46a673cd
Timothee <timothee@timotheehp.rev.sfr.net>
2024-02-11 14:14:05 +0100

    little gate doc

--- src/gate.h
+++ src/gate.h
@@ -3,6 +3,11 @@
 
 #include "ressources.h"
 
+/*
+The player can never go through the gate
+Ghosts can go throught if they are not killed less than GHOST_DEATH_TIME sec ago
+*/
+
 typedef struct Gate Gate;
 struct Gate {
 	Coords coords;
